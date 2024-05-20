d06c15d84b9a48c0e5d764c8ecd30cf6c5c0935c
lanacstudio <120581806+lanac0911@users.noreply.github.com>
2024-05-20 10:36:04 +0800

    feat: 導入 text generator 的初始函式

--- app.py
+++ app.py
@@ -24,7 +24,9 @@ def send_message():
     print('收倒')
     data = request.get_json()
     message = data.get('message')
+    print('===messages1' ,message)
     messages.append(message)
+    print('===messages2' ,message)
 
     # 处理消息并获取模型的预测结果
     doc = nlp(message)
@@ -38,6 +40,22 @@ def send_message():
     
     # 返回预测结果给客户端
     return jsonify({'message': '120w', 'entities': entities}), 200
+
+
+# 淳昇 part
+@app.route('/extend-message', methods=['POST'])
+def extend_message():
+    # 這裡會拿到 (messages)
+    # 1. 結果，如：120w, 開/關...
+    # 2. ner 結果，如：[('昨天', 'Time'), ('冰箱', 'Appliance'), ('使用狀況', 'Query')]
+    data = request.get_json()
+    message = data.get('message')
+    messages.append(message)
+
+   
+    extend_result = '' ### 處理好的內容
+    # 返回预测结果给客户端
+    return extend_result
 
 # 运行应用
 if __name__ == '__main__':
